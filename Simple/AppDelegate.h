d4d3c3d1deccf4a88d2fb578d277525fd770df51
vdemolombe <vdemolombe@imac-de-vdemolombe.local>
2016-01-27 13:52:40 -0800

    Initial Commit

---
+++ Simple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Simple
+//
+//  Created by vdemolombe on 27/01/2016.
+//  Copyright © 2016 vdemolombe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Simple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Simple
+//
+//  Created by vdemolombe on 27/01/2016.
+//  Copyright © 2016 vdemolombe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
